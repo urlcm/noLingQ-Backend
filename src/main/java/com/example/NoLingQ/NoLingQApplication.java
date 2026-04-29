63eb9c532bc6ec0b988b1c327f550e64ed15c808
url <edwinuriel1122@gmail.com>
2026-04-29 16:46:14 -0500

    Commit inicial

---
+++ src/main/java/com/example/NoLingQ/NoLingQApplication.java
@@ -0,0 +1,13 @@
+package com.example.NoLingQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoLingQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoLingQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NoLingQ/NoLingQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoLingQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoLingQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
